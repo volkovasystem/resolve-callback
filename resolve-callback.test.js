477f53fca589f64ff0980e7cc37dc53b28f306c6
Richeve S. Bebebdor <richeve.bebedor@gmail.com>
2020-04-26 01:49:19 +0800

    Added basic test.

--- resolve-callback.test.js
+++ resolve-callback.test.js
@@ -4,8 +4,29 @@ const assert = require( "assert" );
 
 const strictAssert = assert.strict;
 
+const resolveCallback = require( "./resolve-callback.js" );
+
 (
 	async	function( ){
-				
+				const testCallback = (
+					resolveCallback(
+						async	function testCallback( ){
+									return	"Hello World";
+								}
+					)
+				);
+
+				strictAssert
+				.equal(
+					(
+						await	testCallback( )
+					),
+					(
+						"Hello World"
+					),
+					(
+						"Test callback must return 'Hello World'."
+					)
+				);
 			}
 )( );
