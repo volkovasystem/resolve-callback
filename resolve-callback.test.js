8a0abadf08644891c3cbc66211a3980e8795a928
Richeve S. Bebebdor <richeve.bebedor@gmail.com>
2020-04-26 01:52:28 +0800

    Added test for Promise result.

--- resolve-callback.test.js
+++ resolve-callback.test.js
@@ -7,6 +7,32 @@ const strictAssert = assert.strict;
 const resolveCallback = require( "./resolve-callback.js" );
 
 (
+	function( ){
+		const testCallback = (
+			resolveCallback(
+				async	function testCallback( ){
+							return	true;
+						}
+			)
+		);
+
+		strictAssert
+		.equal(
+			(
+							testCallback( )
+				instanceof	Promise
+			),
+			(
+				true
+			),
+			(
+				"Test callback must return a Promise object."
+			)
+		);
+	}
+)( );
+
+(
 	async	function( ){
 				const testCallback = (
 					resolveCallback(
