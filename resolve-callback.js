af4057d100964b336e396c41715a0d986983d116
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2020-09-18 23:53:02 +0800

    Rebuilt production module.

---
+++ resolve-callback.js
@@ -0,0 +1,34 @@
+parcelRequire=function(modules,cache,entry,globalName){var error,previousRequire="function"==typeof parcelRequire&&parcelRequire,nodeRequire="function"==typeof require&&require;function newRequire(name,jumped){if(!cache[name]){if(!modules[name]){var currentRequire="function"==typeof parcelRequire&&parcelRequire;if(!jumped&&currentRequire)return currentRequire(name,!0);if(previousRequire)return previousRequire(name,!0);if(nodeRequire&&"string"==typeof name)return nodeRequire(name);var err=new Error("Cannot find module '"+name+"'");throw err.code="MODULE_NOT_FOUND",err}localRequire.resolve=function resolve(x){return modules[name][1][x]||x},localRequire.cache={};var module=cache[name]=new newRequire.Module(name);modules[name][0].call(module.exports,localRequire,module,module.exports,this)}return cache[name].exports;function localRequire(x){return newRequire(localRequire.resolve(x))}}newRequire.isParcelRequire=!0,newRequire.Module=function Module(moduleName){this.id=moduleName,this.bundle=newRequire,this.exports={}},newRequire.modules=modules,newRequire.cache=cache,newRequire.parent=previousRequire,newRequire.register=function(id,exports){modules[id]=[function(require,module){module.exports=exports},{}]};for(var i=0;i<entry.length;i++)try{newRequire(entry[i])}catch(e){error||(error=e)}if(entry.length){var mainExports=newRequire(entry[entry.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=mainExports:"function"==typeof define&&define.amd?define((function(){return mainExports})):this.resolveCallback=mainExports}if(parcelRequire=newRequire,error)throw error;return newRequire}({Jf2h:[function(require,module,exports){"use strict";
+/*;
+	@license:module:
+		MIT License
+
+		Copyright (c) 2020-present Richeve S. Bebebdor <richeve.bebedor@gmail.com>
+
+		@license:copyright:
+			Richeve S. Bebebdor
+
+			<@license:year-range:2020-present;>
+
+			<@license:contact-detail:richeve.bebedor@gmail.com;>
+		@license:copyright;
+
+		Permission is hereby granted, free of charge, to any person obtaining a copy
+		of this software and associated documentation files (the "Software"), to deal
+		in the Software without restriction, including without limitation the rights
+		to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+		copies of the Software, and to permit persons to whom the Software is
+		furnished to do so, subject to the following conditions:
+
+		The above copyright notice and this permission notice shall be included in all
+		copies or substantial portions of the Software.
+
+		THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+		IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+		FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+		AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+		LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+		OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+		SOFTWARE.
+	@license:module;
+*/var RESOLVE_CALLBACK=Symbol("resolve-callback");module.exports=function resolveCallback(callback){try{if("function"!=typeof callback)throw new Error(["#invalid-callback-parameter;","invalid callback parameter;","@callback:","".concat(callback,";")]);return!0===callback[RESOLVE_CALLBACK]?callback:new Proxy(callback,{apply:function apply(callback,scope,parameterList){return Promise.resolve(callback.apply(scope,parameterList))},get:function get(callback,property,value,target){return property===RESOLVE_CALLBACK||callback[property]}})}catch(error){throw new Error(["#cannot-resolve-callback;","cannot resolve callback;","cannot execute resolve callback;","@error-data:","".concat(error,";")])}}},{}]},{},["Jf2h"]);
